6aae0113be81088c0ab6be3fa96f47f2bf2cd1e2
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-08-18 23:36:46 +0200

    Modal prototype

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class Modal extends Component {
+
   render() {
+
     return (
-      <div className={ this.props.active ? "modal is-active" : "modal is-hidden" }>
-      	{ for (var i in this.props.item ) => i }
-      </div>
+    	<div className={ this.props.active ? "modal__container is-active" : "modal__container is-hidden" } onClick={this.props.toggle}>
+	      <div className="modal">
+
+				{ Object.keys(this.props.item).map((key, index) =>
+			    <h2 key={index}><strong>{key}:</strong> {this.props.item[key]}</h2>
+			  )}
+
+	      </div>
+	   	</div>
     );
   }
 }
